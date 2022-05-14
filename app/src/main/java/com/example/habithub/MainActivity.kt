1410b33f522a20454d28147cb308ea269ba21f5a
KRIPPERGO <vladimirklimov2008@gmail.com>
2022-05-14 15:24:27 +0400

    переделал окно activity_main

--- app/src/main/java/com/example/habithub/MainActivity.kt
+++ app/src/main/java/com/example/habithub/MainActivity.kt
@@ -3,23 +3,47 @@ package com.example.habithub
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textContact: TextView
+    lateinit var textShop: TextView
+    lateinit var buttonStart: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+        initListener()
     }
 
-    fun goToConnect(view: View){
-        startActivity(Intent(this@MainActivity, ConnectActivity::class.java))
+    override fun onBackPressed() {
+        finishAffinity()
     }
 
-    fun start(view: View){
-        startActivity(Intent(this@MainActivity, CreateActivity::class.java))
+    private fun initView() {
+        textContact = findViewById(R.id.NameOfApp)
+        textShop = findViewById(R.id.Shop)
+        buttonStart = findViewById(R.id.ButtonStart)
     }
 
-    fun goToShop(view: View){
-        startActivity(Intent(this@MainActivity, ActivityShop::class.java))
+    private fun initListener() {
+        textContact.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ConnectActivity::class.java))
+            overridePendingTransition(0, 0)
+        }
+        textShop.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ActivityShop::class.java))
+            overridePendingTransition(0, 0)
+        }
+        buttonStart.setOnClickListener {
+            startActivity(Intent(this@MainActivity, CreateActivity::class.java))
+            overridePendingTransition(0, 0)
+        }
     }
 }
